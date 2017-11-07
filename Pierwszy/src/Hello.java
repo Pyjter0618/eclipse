27da88b6cfed7bbed2a584ae97f7d67cb37fbfdb
Pyjter0618 <31891484+Pyjter0618@users.noreply.github.com>
2017-11-07 09:29:48 +0100

    Initial commit

---
+++ Pierwszy/src/Hello.java
@@ -0,0 +1,22 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		int sum=0;
+        
+		for(int i=-10; i<=10; i++) {
+			System.out.print(sum);
+			System.out.print(" ");
+			System.out.println(i);
+			sum=sum+i;
+		} // for...
+		System.out.print("Ostateczny winik:");
+		System.out.print(sum);
+	
+	System.out.println(Integer.MAX_VALUE);
+	System.out.println(Integer.MIN_VALUE);
+	}
+
+}
