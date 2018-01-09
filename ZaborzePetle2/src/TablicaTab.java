e0e96d9a929148d7299507f06a1530ca45e886fb
Pyjter0618 <31891484+Pyjter0618@users.noreply.github.com>
2018-01-09 09:35:06 +0100

    Tablica tab
    asd

---
+++ ZaborzePetle2/src/TablicaTab.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TablicaTab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.print("Ile cghcesz liczb\n" );
+		int k = s.nextInt();
+		int tab[] = newInt[k];
+		for (int i = 0; i < tab.length-1; i++)
+		{
+			zlicz[tab[i]] = zlicz[tab[i]]+1;
+		}
+		for(int i =0; i < tab.length; i++)
+		{
+			System.out.print(zlicz[i]+"");
+		}
+	}
+
+}
